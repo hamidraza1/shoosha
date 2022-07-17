12a3f2fd63cf5f39c4936274f2ba1019aef85817
hamidraza1 <hrazaa306@gmail.com>
2022-07-17 10:59:20 +0200

    added books module

--- apps/main/src/app/app.module.ts
+++ apps/main/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { metaReducers, reducers } from './state/reducers';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     StoreDevtoolsModule.instrument(),
   ],
